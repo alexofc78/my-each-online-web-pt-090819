9ff49d36cfeca986a55a2a8564864f0012f200b9
ALEXANDRA OLIVEROS <alexofc@gmail.com>
2019-09-22 17:26:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,12 @@
-def my_each(word) 
-  while my_each ||
-  puts ""
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
   end
-  # put argument(s) here
-  # code here
+  array
 end
+
 
 #def hello_t(array)
 #  i = 0
